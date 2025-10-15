f76d3787ea35491908b6cd1fc9b7a1434ffc7bcc
kingthemes-dev <kingthemes.dev@gmail.com>
2025-10-15 13:20:54 +0200

    chore: add Cookiebot and GTM snippets (head + noscript body)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -78,6 +78,16 @@ export default function RootLayout({
         <meta name="theme-color" content="#000000" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        {/* Cookiebot */}
+        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="fa168711-f44f-4971-91cf-f37e7337d834" data-blockingmode="auto" type="text/javascript"></script>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{__html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TJSTQLNM');
+        `}} />
         {/* Preconnect to WordPress for faster API calls */}
         <link rel="preconnect" href="https://qvwltjhdjw.cfolks.pl" crossOrigin="" />
         <link rel="dns-prefetch" href="https://qvwltjhdjw.cfolks.pl" />
@@ -95,6 +105,8 @@ export default function RootLayout({
                     className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
                     suppressHydrationWarning={true}
                   >
+                        {/* Google Tag Manager (noscript) */}
+                        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJSTQLNM" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
                         <ErrorBoundary>
                           <TopBar />
                           <Header />
