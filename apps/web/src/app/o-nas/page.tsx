d01a6186ec769e9be32ce3e831987dcd092296f6
kingthemes-dev <kingthemes.dev@gmail.com>
2025-10-21 01:45:46 +0200

    ✨ [O nas] - Zmniejszono padding sekcji z ikonami, usunięto nadmiar miejsca do stopki

--- apps/web/src/app/o-nas/page.tsx
+++ apps/web/src/app/o-nas/page.tsx
@@ -88,7 +88,7 @@ export default function AboutPage() {
         </section>
 
         {/* Features Section */}
-        <section className="py-16">
+        <section className="py-8">
           <PageContainer>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
               {/* Konkurencyjne ceny */}
